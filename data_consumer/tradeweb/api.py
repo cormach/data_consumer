dc096886f6b4d17505878aac76f055f64fab966c
Cormac Hollingsworth <16384725+cormach@users.noreply.github.com>
2023-08-18 07:49:40 +0000

    selenium webdriver builds

--- data_consumer/tradeweb/api.py
+++ data_consumer/tradeweb/api.py
@@ -1,16 +1,19 @@
 from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 
 # Create a new instance of the Chrome browser - TODO: Add the Chromedriver Address
-driver = webdriver.Chrome()
-download_directory = '/path/to/your/download/directory'
 
 # Configure Chrome options
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')  # May be needed depending on your environment
 chrome_options.add_argument('--headless')    # Run in headless mode if desired
 chrome_options.add_argument('--disable-gpu') # Disable GPU acceleration if needed
+download_directory = '.'
 
 # Set the download directory option
 chrome_options.add_argument(f'--download.default_directory={download_directory}')
+driver = webdriver.Chrome(options=chrome_options)
+
+
+driver.quit()
